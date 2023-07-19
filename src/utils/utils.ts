6dd85883297b9c663703c45ca69c54bb2afa3d13
VitoZMX <Vi-69@yandex.ru>
2023-07-19 17:59:09 +0300

    fix: create new Date for firefox

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,9 @@
-export function subtractWorkday(date: Date) {
-    const dayOfWeek = date.getDay() // получаем день недели от 0 (воскресенье) до 6 (суббота)
+export function subtractWorkday(date: string) {
+    const x = new Date(Date.parse(date))
+
+    const dayOfWeek = x.getDay() // получаем день недели от 0 (воскресенье) до 6 (суббота)
     const daysToSubtract = dayOfWeek === 1 ? 3 : 1 // если понедельник, то отнимаем 3 дня, иначе 1 день
-    const newDate = new Date(date)
-    newDate.setDate(date.getDate() - daysToSubtract) // отнимаем дни от даты
-    return newDate.toISOString()
+    const newDate = new Date(x)
+    newDate.setDate(x.getDate() - daysToSubtract) // отнимаем дни от даты
+    return new Date(newDate).toISOString()
 }
