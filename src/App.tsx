f32dac9cff136a997a363035598b190505c19d76
VitoZMX <Vi-69@yandex.ru>
2023-07-19 11:45:52 +0300

    refactor: change structure of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './App.scss'
+import {NavBar} from './component/Navbar/NavBar'
+import {HashRouter, Navigate, Route, Routes} from 'react-router-dom'
+import {CurrenciesPage} from './component/CurrenciesPage/CurrenciesPage'
+import {CurrencyFullDataPage} from './component/CurrencyFullDataPage/CurrencyFullDataPage'
+import {currenciesAPI} from './API/currenciesAPI'
+import {RateType} from './types/types'
+import {Preloader} from './component/common/Preloader'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    const [loading, setLoading] = useState(true)
+    const [rateDay, setRateDay] = useState<RateType[]>([])
+
+    useEffect(() => {
+        currenciesAPI.getAllCurrenciesRateDaily().then((data) => {
+
+            Promise.all(
+                data.map((item) => {
+                    return currenciesAPI.getPreviousRateOneCurrencies(item.Cur_ID, item.Date)
+                        .then((res) => {
+                            item.yesterday = res
+                            item.exchangeRateDifference = item.Cur_OfficialRate - res.Cur_OfficialRate
+                        })
+                })
+            ).then(() => {
+                setRateDay(data)
+                setLoading(false)
+                console.log(data)
+            })
+        })
+    }, [])
+
+    if (loading) return <Preloader/>
+
+    return (
+        <HashRouter>
+            <NavBar/>
+            <Routes>
+                <Route path="/currencies" element={<CurrenciesPage rateDay={rateDay}/>}/>
+                <Route path="/currency/:id" element={<CurrencyFullDataPage/>}/>
+                <Route path="/currency" element={<CurrencyFullDataPage/>}/>
+                <Route path="/*" element={<Navigate to={'/currencies'}/>}/>
+            </Routes>
+        </HashRouter>
+    )
 }
-
-export default App;
