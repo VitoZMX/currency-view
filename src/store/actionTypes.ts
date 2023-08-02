733a906dbc8faf98f6036734bc197c2fd2a4d761
VitoZMX <Vi-69@yandex.ru>
2023-08-02 14:12:50 +0300

    feat: add action types

---
+++ src/store/actionTypes.ts
@@ -0,0 +1,8 @@
+export const SET_ALL_CUR = 'SET_ALL_CUR'
+export const SET_CUR_DATA = 'SET_CUR_DATA'
+export const SET_CURRENCY_RATE = 'SET_CURRENCY_RATE'
+export const SET_DATE_PERIOD = 'SET_DATE_PERIOD'
+export const SET_ERROR = 'SET_ERROR'
+export const SET_COUNT_MONTH = 'SET_COUNT_MONTH'
+
+export const INITIALIZED_SUCCESS = 'INITIALIZED_SUCCESS'
